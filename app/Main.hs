08b047a3197cd2e2f71bc8ad7c6d2dea28718749
Artemii Kuzmin <artsuper472@gmail.com>
2025-11-07 15:00:36 +0300

    changed data types

--- app/Main.hs
+++ app/Main.hs
@@ -3,8 +3,4 @@ module Main (main) where
 import Bag
 
 main :: IO ()
-main = do
-    let bag = newBag
-    let bag' = put "hello" bag
-    let bag'' = put "hello" bag'
-    print bag''
+main = return ()
--- src/Bucket.hs
+++ src/Bucket.hs
@@ -2,13 +2,6 @@ module Bucket (
     Bucket (..), updateBuckets
 ) where
 
-import Node
+data Status = Empty | Occupied | Deleted deriving (Show, Eq)
 
-newtype Bucket a = Bucket (Node a) deriving (Show, Eq)
-
-updateBuckets :: [Bucket a] -> Int -> Node a -> [Bucket a]
-updateBuckets buckets index newNode = before ++ [Bucket $ insertNode oldNode newNode] ++ after 
-    where
-        Bucket oldNode = buckets !! index
-        before = take index buckets
-        after = drop (index + 1) buckets
+newtype Bucket k v = Bucket k v Status deriving (Show, Eq)
